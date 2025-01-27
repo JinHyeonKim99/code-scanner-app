5a1820e3a9ccf37e9f2ffaa1949df761bba67bac
SibangGyeon <briankim0695@gmail.com>
2025-01-27 19:46:54 +0900

    feat : 코드 스캐너 MainActivity 구현, AppApplication -> Manifest 등록

---
+++ app/src/main/java/com/example/code_scanner_app/AppApplication.kt
@@ -0,0 +1,8 @@
+package com.example.code_scanner_app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AppApplication : Application() {
+}
--- app/src/main/java/com/example/code_scanner_app/MainActivity.kt
+++ app/src/main/java/com/example/code_scanner_app/MainActivity.kt
@@ -4,44 +4,38 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.code_scanner_app.ui.theme.CodescannerappTheme
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.rememberNavController
+import com.example.code_scanner_app.presentation.main_screen.MainScreen
+import com.example.code_scanner_app.presentation.main_screen.MainScreenViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
-            CodescannerappTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            val navController = rememberNavController()
+
+            val mainScreenViewModel: MainScreenViewModel = hiltViewModel()
+
+            val mainScreenState by mainScreenViewModel.state.collectAsState()
+
+            MainScreen(
+                state = mainScreenState,
+                onButtonClick = { mainScreenViewModel.onButtonClick() },
+            )
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    CodescannerappTheme {
-        Greeting("Android")
-    }
+fun MainActivityPreview() {
+    MainScreen()
 }
