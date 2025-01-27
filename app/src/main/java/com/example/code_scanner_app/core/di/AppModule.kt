fd4f73c483b246095bb1245a2648ada33132c34a
SibangGyeon <briankim0695@gmail.com>
2025-01-27 19:45:03 +0900

    feat : 코드 스캐너 Hilt DI 추가 - AppModule 작성 및 Hilt 적용

---
+++ app/src/main/java/com/example/code_scanner_app/core/di/AppModule.kt
@@ -0,0 +1,40 @@
+package com.example.code_scanner_app.core.di
+
+import com.example.code_scanner_app.data.data_source.MockCodeDataSource
+import com.example.code_scanner_app.data.data_source.MockImageDataSource
+import com.example.code_scanner_app.data.repository.CodeRepositoryImpl
+import com.example.code_scanner_app.data.repository.ImageRepositoryImpl
+import com.example.code_scanner_app.domain.data_source.CodeDataSource
+import com.example.code_scanner_app.domain.data_source.ImageDataSource
+import com.example.code_scanner_app.domain.repository.CodeRepository
+import com.example.code_scanner_app.domain.repository.ImageRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class AppModule {
+
+    @Singleton
+    @Binds
+    abstract fun provideCodeDataSource(
+        codeDataSource: MockCodeDataSource,
+    ): CodeDataSource
+
+    @Singleton
+    @Binds
+    abstract fun provideImageDataSource(
+        imageDataSource: MockImageDataSource,
+    ): ImageDataSource
+
+    @Singleton
+    @Binds
+    abstract fun provideCodeRepository(codeRepository: CodeRepositoryImpl): CodeRepository
+
+    @Singleton
+    @Binds
+    abstract fun provideImageRepository(imageRepository: ImageRepositoryImpl): ImageRepository
+}
